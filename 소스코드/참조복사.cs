7a280e76f9679486e64deb42ec91b82222f4eb8e
ehdrms0131 <ehdrms1182@gmail.com>
2020-11-10 21:12:58 +0900

    참조복사
    Reference

---
+++ 소스코드/참조복사.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game10_15.참조에의한_매개변수_전달
+{
+    class 참조복사
+    {
+        static void AddValue(ref int value,int number)//값복사
+        {
+            value += number;
+        }
+        static void Main()
+        {
+            //참조복사
+            //call by Reference:참조에 의한 호출
+            //pass by Reference:참조에 의한 전달
+            int num = 10;
+            AddValue(ref num, 10);//변하지않음
+            Console.WriteLine(num);
+            //ref->c#
+            //c->pointer
+            //컴파일 하면 20
+        }
+    }
+}
---
+++ 소스코드/참조복사2ref.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game10_15.참조에의한_매개변수_전달
+{
+    class 참조복사2ref
+    {
+        static ref int GetRefData(ref int r_value)
+        {
+            return ref r_value;//반환 타입 (레퍼런스 인트)리턴값:레퍼런스밸류(주소값 반환)
+            //가리킴보다는 공유,참조
+        }
+        static void Main()
+        {
+            //로컬함수:main안에 함수 더 있는것 마찬가지로 일회성 강함
+            //람다함수:일회성이 강함
+            int a = 10;
+            int b = GetRefData(ref a);
+            Console.WriteLine(b);
+            //레퍼런스
+            Console.WriteLine(a);
+            if (a == b)
+            {
+                Console.WriteLine("같음");
+            }
+            else
+            {
+                Console.WriteLine("같음");
+            }
+        }
+    }
+}
