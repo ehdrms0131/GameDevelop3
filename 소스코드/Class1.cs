d223fe67d150e6c0f9774582bad9b0302b8ea530
ehdrms0131 <ehdrms1182@gmail.com>
2020-11-17 23:42:12 +0900

    분할클래스
    with gameDevelop2
    partial class Beginner

---
+++ 소스코드/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1
+{
+    partial class Class1
+    {
+        private string Name { get; set; }
+        public void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    partial class Class1
+    {
+        public void Print2()
+        {
+            Console.WriteLine("싸발");
+        }
+    }
+
+    class mainClass
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
